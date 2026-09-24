bde40033274422dd66ce060fb48afd086e15d939
Givaro-Ananta <givaroananta02@gmail.com>
2026-09-24 22:24:57 +0700

    refresh

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark scroll-smooth">
-      <body className={`${inter.className} bg-gray-950 text-gray-100 antialiased overflow-x-hidden`}>
+      <body className={`${inter.className} bg-gray-950 text-gray-100 antialiased`}>
         {children}
         <Analytics />
       </body>
